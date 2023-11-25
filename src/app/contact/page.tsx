62e8c6fda76d71e06aa9d6b9620a5e6a75346453
whcender <heilender31@gmail.com>
2023-11-25 19:59:34 +0300

    contact center

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaVimeo, FaSpotify } fro
 const page = () => {
     return (
         <div className='lg:w-[1200px] h-[500px] m-auto px-4'>
-            <h4 className='font-semibold'>İletişime Geçin</h4>
+            <h4 className='font-semibold text-center'>İletişime Geçin</h4>
             <div className='w-[300px]  m-auto flex items-center flex-wrap justify-center mt-20'>
                 <div className='border w-max p-4 hover:bg-black hover:text-white'>
                     <Link href={"https://www.facebook.com/thekeremkorkmaz"}>
