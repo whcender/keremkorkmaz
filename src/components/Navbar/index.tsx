34a6a21a71cd324fb78ae6ed08aeb88cb6a2694b
whcender <heilender31@gmail.com>
2023-11-25 19:26:41 +0300

    nav be

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
+import { FaFilm } from "react-icons/fa";
+
 import React from 'react'
 
 const index = () => {
   return (
-    <div className='flex flex-col gap-3 text-center mt-3 mb-10 max-lg:mb-24 '>
+    <div className='flex flex-col gap-3 text-center items-center mt-3 mb-10 max-lg:mb-24 '>
         <Link href={"/"}>
         <h1 className='text-3xl font-black max-lg:text-2xl'>Kerem Korkmaz</h1></Link>
+        <FaFilm size={30}/>
         <p className='text-gray-900 uppercase text-sm max-lg:text-xs font-semibold'>Yazar - Senarist - Radyocu - Yönetmen</p>
     </div>
   )
