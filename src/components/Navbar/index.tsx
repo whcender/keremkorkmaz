84bbd11fba3629db6f40c7aeea16331287fa4f13
whcender <heilender31@gmail.com>
2023-11-26 15:06:26 +0300

    yellow

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
   return (
     <div className='flex flex-col gap-3 text-center items-center mt-3 mb-10 max-lg:mb-24 '>
         <Link href={"/"}>
-        <h1 className='text-3xl font-black max-lg:text-2xl'>Kerem Korkmaz</h1></Link>
+        <h1 className='text-3xl font-black max-lg:text-2xl text-yellow-400'>Kerem Korkmaz</h1></Link>
         <FaFilm size={30}/>
         <p className='text-gray-900 uppercase text-sm max-lg:text-xs font-semibold'>Yazar - Senarist - Radyocu - Yönetmen</p>
     </div>
