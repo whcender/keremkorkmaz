15bd21412526267b37c3dea70a6dac08ae87d9fc
whcender <heilender31@gmail.com>
2023-11-26 14:43:01 +0300

    contact finish

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import React from 'react'
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaVimeo, FaSpotify } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaVimeo, FaSpotify, FaImdb  } from "react-icons/fa";
+import { ImSoundcloud2 } from "react-icons/im";
+import { SiMedium } from "react-icons/si";
 import Main from '@/components/Main';
 
 const page = () => {
@@ -31,6 +33,18 @@ const page = () => {
                 <div className='border w-max p-4 hover:bg-black hover:text-white'>
                     <Link target="_blank" href={"https://www.facebook.com/thekeremkorkmaz"}>
                         <FaSpotify size={60} /></Link>
+                </div>
+                <div className='border w-max p-4 hover:bg-black hover:text-white'>
+                    <Link target="_blank" href={"https://medium.com/@keremkorkmaz"}>
+                        <SiMedium size={60} /></Link>
+                </div>
+                <div className='border w-max p-4 hover:bg-black hover:text-white'>
+                    <Link target="_blank" href={"https://www.imdb.com/user/ur68685099/?ref_=nb_usr_prof_0&fbclid=IwAR0H5BzY69b8Eb5eFBallSTEtqPT6Tbg8T833rDTmqd3sAp1VofRcFTpZy0"}>
+                        <FaImdb  size={60} /></Link>
+                </div>
+                <div className='border w-max p-4 hover:bg-black hover:text-white'>
+                    <Link target="_blank" href={"https://soundcloud.com/thekeremkorkmaz"}>
+                        <ImSoundcloud2  size={60} /></Link>
                 </div>
             </div>
             <div className='flex items-center justify-center mt-10 gap-5 max-lg:text-sm mb-10'>
