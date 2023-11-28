560063fa8439d1aea2dc06b5f5ddd579e369df60
whcender <heilender31@gmail.com>
2023-11-28 13:36:08 +0300

    nav links

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,6 +5,7 @@ const index = () => {
   return (
     <div className='flex items-center font-medium uppercase justify-center mt-10 max-lg:mt-2 max-lg:gap-3 max-lg:flex-col mb-10 text-lg'>
       <Link className='border rounded-lg border-black bg-black text-white w-36 max-lg:w-[80%] text-center hover:font-semibold' href={"/"}>Anasayfa</Link>
+      <div className='h-[2px] w-2 bg-black max-lg:hidden'></div>
       <Link className='border rounded-lg border-black bg-black text-white w-36 max-lg:w-[80%] text-center hover:font-semibold' href={"/about"}>Hakkımda</Link>
       <div className='h-[2px] w-2 bg-black max-lg:hidden'></div>
       <Link className='border rounded-lg border-black bg-black text-white w-36 max-lg:w-[80%] text-center hover:font-semibold' href={"/myproject"}>Projelerim</Link>
