ca5bea76fcffcc73a5ca31e38a18ae797290b0b8
whcender <heilender31@gmail.com>
2023-12-03 18:10:37 +0300

    project v1

--- src/app/myproject/page.tsx
+++ src/app/myproject/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Projects from '@/components/Projects'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Projects />
+    </div>
   )
 }
 
