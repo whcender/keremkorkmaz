bd4fa1f7746c9cba461895a37787496b1aeb2b5f
Fatih Aslan <82971212+whcender@users.noreply.github.com>
2024-04-14 01:17:18 +0300

    Update index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -4,7 +4,7 @@ const index = () => {
   return (
     <div className='flex gap-6 w-[1200px] m-auto items-start justify-center max-lg:flex-col max-lg:w-[80%]'>
         <div>
-            <p className='w-[500px] max-lg:w-[100%] max-lg:text-sm '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam assumenda, consequatur fugiat dolores non fugit sit optio nam accusantium saepe? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium suscipit fuga illum iusto exercitationem voluptates odit praesentium id consequatur hic nemo at eveniet sapiente dolore explicabo expedita quia distinctio eum doloribus, aliquid ipsum quam repudiandae. Perferendis vero beatae magni amet repellat qui ipsum eveniet?</p>
+            <p className='w-[500px] max-lg:w-[100%] max-lg:text-sm '>Merhaba, ben Kerem. Sakarya Kent FM'in kurucusu, radyo sunucusu ve genel yayın yönetmeniyim. Aynı zamanda Resital Dijital yapım şirketinde kısa ve uzun metrajlı filmler, internet dizileri ve Late Night Talk Show programları ,podcast yapımları ve radyo tiyatrosu yapımları üretiyorum. Kendi kurduğum Hamur Humour Komedi Kulübü'nde The Comedy Clubhouse ve Açık Mikrofon geceleri düzenliyorum ve kendi tek kişilik gösterimi sergiliyorum. Ayrıca, Dramatik Sanatlar Akademisi'nde kısa film yapımı, senaryo yazımı ve tiyatro oyunculuğu gibi teknik eğitimler veriyorum.</p>
         </div>
         <img className='w-[400px] clipped-image' src="/main.jpg" alt="kerem" />
     </div>
